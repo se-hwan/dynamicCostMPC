302fce48f790f72f3c0de5afbe5820827ba2955b
Se Hwan <sehwan@mit.edu>
2022-01-10 13:43:41 -0500

    file cleanup + data plotting

--- bayesianOptimization/RS-bayesianOptimization.py
+++ bayesianOptimization/RS-bayesianOptimization.py
@@ -1,4 +1,6 @@
+import sys
 import lcm
+sys.path.insert(0, './lcm')
 from dcmpc_reward_lcmt import dcmpc_reward_lcmt
 from dcmpc_parametrization_lcmt import dcmpc_parametrization_lcmt
 from bayes_opt import BayesianOptimization
@@ -33,6 +35,9 @@ def publishParameters(lc, msg):  # publishes parameters to Robot-Software
     msg.test = 1.0;
     lc.publish("DCMPC_PARAM", msg.encode())
 
+# TODO: Make this general to variety of message types, not just Q diagonal - sehwan
+
+
 lc = lcm.LCM()
 subscription = lc.subscribe("DCMPC_REWARDS", my_handler)    # incoming messages from Robot-Software (rewards)
 msg = dcmpc_parametrization_lcmt()                          # messages to output to Robot-Software (parameters)
@@ -53,8 +58,8 @@ optimizer = BayesianOptimization(f=RobotSoftwareSimulation,
                                  pbounds=p_bounds,
                                  verbose=2,
                                  random_state=1)
-optimizer.maximize(init_points=1, n_iter=1)
-logger = JSONLogger(path="./logs.json")
+optimizer.maximize(init_points=0, n_iter=0)
+logger = JSONLogger(path="./data/logs.json")
 optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-optimizer.maximize(init_points=2, n_iter=5)
+optimizer.maximize(init_points=10, n_iter=500)
 
---
+++ bayesianOptimization/data/bo_data.m
@@ -0,0 +1,29 @@
+clear; clc;
+
+%% load and parse json data
+fname = 'fwd_running.json'; 
+fid = fopen(fname); 
+raw = fread(fid,inf); 
+str = char(raw'); 
+fclose(fid);
+idx_entries = find(raw == 10);
+idx_entries = [1; idx_entries];
+
+simulations = cell(length(idx_entries)-1, 1);
+target_val = zeros(length(simulations), 1);
+param_val = zeros(length(simulations), 12);
+
+entry = jsondecode(str(idx_entries(1):idx_entries(2)));
+param_names = fieldnames(entry.params);
+
+for i = 1:length(idx_entries)-1
+    entry = jsondecode(str(idx_entries(i):idx_entries(i+1)));
+    target_val(i) = entry.target;
+    for j = 1:12
+        param_val(i, j) = entry.params.(param_names{j});
+    end
+end
+
+
+%% plotting and analysis
+max(target_val)
