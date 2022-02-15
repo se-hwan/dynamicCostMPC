9c4e618efda4cfe723b3696e8d2961b9dc830959
Se Hwan <sehwan@mit.edu>
2022-02-15 17:11:17 -0500

    added breakpoint if maximum reward is within tolerance of latest parameter evaluation

--- bayesianOptimization/dcmpc-training.py
+++ bayesianOptimization/dcmpc-training.py
@@ -7,7 +7,7 @@ sys.path.append('./lcm')
 sys.path.append('../../config')
 from dcmpc_reward_lcmt import dcmpc_reward_lcmt
 from dcmpc_parametrization_lcmt import dcmpc_parametrization_lcmt
-from bayes_opt import BayesianOptimization
+from bayes_opt import BayesianOptimization, UtilityFunction
 from bayes_opt.logger import JSONLogger
 from bayes_opt.event import Events
 
@@ -59,9 +59,16 @@ with open('./data/cmd_sweep.csv',newline='') as csvfile:
 # choose desired rate of increase in commands
 ramp_rate = [0.5, 0.5, 1.0]
 
+# number of initial (random) points and maximum points to evalute
+iter_rand = 50
+iter_max = 250
+
 print('Sweep over velocity commands successfully loaded! ', command_count, ' velocity commands are prepared.')
 print('Commands set to increase at rate: ', ramp_rate)
 print('Beginning Bayesian optimization process...')
+print('Random initial points to evaluate: ', iter_rand)
+print('Maximum points to evaluate: ', iter_max)
+
 
 for cmd_idx in range(0, command_count):
 
@@ -98,14 +105,38 @@ for cmd_idx in range(0, command_count):
                                      pbounds=p_bounds,
                                      verbose=2,
                                      random_state=1)
-    optimizer.maximize(init_points=0, n_iter=0)
 
     # setup data filenames
     file_name_BO = folder_name_BO + "/cmd_sweep_" + str(cmd_idx+1)
     file_name_RS = folder_name_RS + "/cmd_sweep_" + str(cmd_idx+1) 
     logger = JSONLogger(path=file_name_BO+'.json')
     optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-    optimizer.maximize(init_points=50, n_iter=250, acq="ucb", kappa=0.5)
+    
+    utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
+
+    optimizer._prime_queue(iter_rand)
+
+    eps = 1e-3
+    iteration = 0
+    max_target = 999
+    while not optimizer._queue.empty or iteration < iter_max:
+        target = 0
+        try:
+            x_probe = next(optimizer._queue)
+            optimizer.probe(x_probe, lazy=False)
+            print("Max value from random search: %6.5f" % (optimizer.max['target']))
+        except StopIteration:
+            utility.update_params()
+            x_probe = optimizer.suggest(utility)
+            target = RobotSoftwareSimulation(**x_probe)
+            optimizer.register(params=x_probe, target=target)
+            print("Iteration: %3d Target value: %6.5f" % (iteration+1, target))
+            iteration += 1
+        if (abs(max_target - target) < eps and iteration > 20):
+            print("Improvement tolerance reached, exiting loop...")
+            break
+        max_target = optimizer.max['target']
+    optimizer.dispatch(Events.OPTIMIZATION_END)
 
     print("Bayesian optimization complete! Saving results...")
     os.replace('./data/RS/DCMPC_sim_data.bin', file_name_RS+'.bin')
@@ -121,3 +152,18 @@ for cmd_idx in range(0, command_count):
     steady_state = 0
 
 
+    
+'''
+    for idx in range(iter_rand):
+        rand_point = next(optimizer._queue)
+        optimizer.probe()
+        target = RobotSoftwareSimulation(**rand_point)
+        optimizer.register(params=rand_point, target=target)
+        print("Iteration: %3d Target value: %6.5" % (idx, target))
+    
+    for idx in range(iter_rand, iter_max):
+        next_point = optimizer.suggest(utility)
+        target = RobotSoftwareSimulation(**next_point)
+        optimizer.register(params=next_point, target=target)
+        print("Iteration: %3d Target value: %6.5" % (idx, target))
+'''
